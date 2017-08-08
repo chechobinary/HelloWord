41af0f9a43d619bc8415760d17a0891d858cab33
Sergio Garcia Belenguer <sgarciabelenguer@gmail.com>
2017-08-08 21:55:57 +0100

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.ws.client.BindingProviderProperties;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        System.out.println(BindingProviderProperties.REQUEST_TIMEOUT);
+    }
+
+
+}
